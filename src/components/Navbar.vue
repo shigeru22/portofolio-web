65f05a850e7525b126bb5306e6bc25cd2b9668b6
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2021-07-30 10:48:40 +0700

    Add and reference Heroicons, and change coloring

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-4 flex justify-center font-sans">
-    <router-link class="p-4 pl-6 pr-6 border-b-2 border-green-400 hover:border-green-300" v-for="link in links" :key="link.link" :to="link.link" active-class="border-green-600">{{ link.name }}</router-link>
+    <router-link class="p-4 pl-6 pr-6 border-b-2 border-green-400 hover:border-green-600" v-for="link in links" :key="link.link" :to="link.link" active-class="border-green-800">{{ link.name }}</router-link>
   </div>
 </template>
 
