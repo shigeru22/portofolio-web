02bd27b7fd83331fc348f9c48296bf07271e819c
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2021-11-05 18:20:51 +0700

    Remove unused modules

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHome, faTable, faIdCard, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import { faTwitter, faLinkedin, faGithub, faPhp, faBootstrap, faUnity, faAndroid, faVuejs } from "@fortawesome/free-brands-svg-icons";
-import { MotionPlugin } from "@vueuse/motion";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/index.css";
@@ -26,5 +25,4 @@ library.add(
 createApp(App)
 	.component("font-awesome-icon", FontAwesomeIcon)
 	.use(router)
-	.use(MotionPlugin)
 	.mount("#app");
