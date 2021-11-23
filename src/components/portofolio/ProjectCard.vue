4db7154da3adb34cc2bd2500540c720bdc4e6fe3
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2021-11-23 11:13:31 +0700

    Create smaller view for project list and cards

--- src/components/portofolio/ProjectCard.vue
+++ src/components/portofolio/ProjectCard.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="flex flex-col justify-center items-start md:items-center w-full md:w-75 h-25 md:h-100 p-8 bg-white shadow-lg rounded-lg space-y-4 transform transition-transform hover:scale-105">
-	<div class="flex items-center md:block w-full h-4/5 text-center space-x-6 md:space-x-0 md:space-y-4">
-		<img :src="require(`@/assets/${ img }`)" :alt="name" class="w-10 md:w-40 h-10 md:h-40 mx-auto" />
-		<div class="flex-grow md:space-y-2">
+<div class="flex flex-col justify-center items-start md:items-center w-full md:w-64 h-full px-4 bg-white shadow-lg rounded-lg space-y-4 transform transition-transform hover:scale-105">
+	<div class="flex md:flex-col md:justify-center items-center w-full h-4/5 my-8 text-center space-x-6 md:space-x-0 md:space-y-4">
+		<img :src="require(`@/assets/${ img }`)" :alt="name" class="w-10 md:w-32 xl:w-40 h-10 md:h-32 xl:h-40 mx-auto" />
+		<div class="md:space-y-2">
 			<h2 class="font-bold text-xl md:text-2xl" :style="{ color: color }">{{ name }}</h2>
-			<p class="font-semibold text-base md:text-lg leading-snug" :style="{ color: color, opacity: 0.85 }">{{ desc }}</p>
+			<p class="font-semibold text-base md:text-lg" :style="{ color: color, opacity: 0.85 }">{{ desc }}</p>
 		</div>
 	</div>
 </div>
