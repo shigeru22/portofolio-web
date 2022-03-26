800ec2c18eb3c172ab7b7712971012eebc2ff33e
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-26 23:24:09 +0700

    Implement project items retrieval route

---
+++ pages/api/projects/index.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Deta } from "deta";
+import { HTTPStatus, HTTPMethod } from "../../../utils/http";
+import { LogSeverity, log } from "../../../utils/log";
+import { IMessageData } from "../../../types/api/message";
+import { IProjectItemData } from "../../../types/project-item";
+import { IProjectItemsResponseData } from "../../../types/api/projects";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<IMessageData>) {
+	if(req.method !== HTTPMethod.GET) {
+		const data: IMessageData = {
+			message: "The requested method is not allowed."
+		};
+
+		res.status(HTTPStatus.METHOD_NOT_ALLOWED).json(data);
+		return;
+	}
+
+	if(typeof(process.env.DETA_PROJECT_KEY) === "undefined" || process.env.DETA_PROJECT_KEY === "") {
+		const data: IMessageData = {
+			message: "Project key is not yet assigned."
+		};
+
+		res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json(data);
+		return;
+	}
+
+	const deta = Deta(process.env.PROJECT_KEY);
+	const db = deta.Base("portfolio-items");
+
+	try {
+		const fetchResult = (await db.fetch()).items as unknown as IProjectItemData[];
+		const data: IProjectItemsResponseData = {
+			message: "Data retrieved successfully.",
+			data: fetchResult
+		};
+
+		res.status(HTTPStatus.OK).json(data);
+	}
+	catch (e) {
+		if(e instanceof Error) {
+			log(LogSeverity.ERROR, "projects/handler", `${ e.name }: ${ e.message }`);
+		}
+		else {
+			log(LogSeverity.ERROR, "projects/handler", "Unknown error occurred.");
+		}
+
+		const data: IMessageData = {
+			message: "Data insertion failed."
+		};
+
+		res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json(data);
+	}
+}
--- types/api/projects.ts
+++ types/api/projects.ts
@@ -1,6 +1,15 @@
 import { IKeyPOSTData } from "./key";
 import { IProjectItemData } from "../project-item";
+import { IMessageData } from "./message";
 
 export interface IProjectItemPOSTData extends IKeyPOSTData {
-	data: IProjectItemData
+	data: IProjectItemData;
+}
+
+export interface IProjectItemResponseData extends IMessageData {
+	data: IProjectItemData;
+}
+
+export interface IProjectItemsResponseData extends IMessageData {
+	data: IProjectItemData[];
 }
