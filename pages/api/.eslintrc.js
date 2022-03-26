4f0b9a536ffef091dcd2bd8b9a5c57c94b6a5e23
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-26 11:33:58 +0700

    Add ESLint configs and lint all files

---
+++ pages/api/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"extends": "../../.eslintrc.js",
+	"rules": {
+		/* possible problems */
+		"no-console": "off",
+
+		/* layout and formatting */
+		"operator-linebreak": [ "warn", "none" ]
+	}
+};
