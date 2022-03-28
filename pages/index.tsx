d87e130e8d7fd5a574a071e556cd10cef656b42c
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-29 00:05:03 +0700

    Implement footer for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import ProfilePicture from "../components/profile-picture";
+import Config from "../config.json";
 
 function Home() {
 	return (
-		<>
+		<div className="flex flex-col w-full h-full">
 			<div className="flex justify-center items-center h-full">
 				<ProfilePicture src="/profpic.jpg" />
 			</div>
-		</>
+			<div className="flex justify-center items-center py-8">
+				<p className="font-medium text-2xl text-light-20">
+					Profile picture by<a href={ Config.img.link } className="text-light-0">{ Config.img.creator }</a>.
+				</p>
+			</div>
+		</div>
 	);
 }
 
