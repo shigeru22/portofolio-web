b312246de125e1937514aa6e96d9309128f05038
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-29 01:32:41 +0700

    Create language graph component

---
+++ components/language-graph.tsx
@@ -0,0 +1,44 @@
+import { ValueError } from "../types/error";
+import { ILanguageGraphData } from "../types/components/language-graph";
+
+interface ILanguageGraphProps {
+	data: ILanguageGraphData[];
+}
+
+function LanguageGraph({ data }: ILanguageGraphProps) {
+	interface IGraphItemProps {
+		percentage: number | string;
+		label: string;
+	}
+
+	function GraphItem({ percentage, label } : IGraphItemProps) {
+		const heightPercentage = typeof(percentage) === "number" ? percentage : parseInt(percentage, 10);
+
+		if(heightPercentage <= 0 || heightPercentage >= 100) {
+			throw new ValueError("Percentage must be between (including) 0 and 100.");
+		}
+
+		return (
+			<div className="flex flex-col gap-y-2 w-14 h-full">
+				<div className="flex flex-grow justify-center items-end">
+					<div className="w-2 bg-light-20" style={ { height: `${ heightPercentage }%` } } />
+				</div>
+				<div className="flex justify-center items-center w-full h-8">
+					<p className="font-medium text-lg text-light-0">{ label }</p>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className="flex h-48 border-b-2 border-light-0">
+			{
+				data.map(item => (
+					<GraphItem key={ item.id } percentage={ item.percentage } label={ item.label } />
+				))
+			}
+		</div>
+	);
+}
+
+export default LanguageGraph;
