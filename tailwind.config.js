2ec468da2405644ef15e471e83fb25919a0f4129
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-28 23:40:44 +0700

    Update Tailwind color palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,26 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}"
 	],
 	theme: {
+		colors: {
+			"black": "#000000",
+			"white": "#ffffff",
+			"light": {
+				"0": "#042a2b",
+				"20": "#4d9361",
+				"40": "#93ff96",
+				"60": "#d0ffb7",
+				"80": "#f2f5de",
+				"100": "#fceff9"
+			},
+			"dark": {
+				"0": "#030b0d",
+				"20": "#092327",
+				"40": "#032f36",
+				"60": "#0f3b42",
+				"80": "#559741",
+				"100": "#99e0f2"
+			}
+		},
 		fontFamily: {
 			"sans": [ "Jost", "sans-serif" ]
 		},
