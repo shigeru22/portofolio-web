cbebe28a109eed4b1fb592f29bd5a428820d14b4
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-29 01:01:07 +0700

    Modify content container height

--- components/content-container.tsx
+++ components/content-container.tsx
@@ -4,7 +4,7 @@ interface IContentContainerProps {
 
 function ContentContainer({ children }: IContentContainerProps) {
 	return (
-		<div className="w-full h-full space-y-2 pl-6 border-l-2 border-light-0">
+		<div className="w-full space-y-2 pl-6 border-l-2 border-light-0">
 			{ children }
 		</div>
 	);
