0061a04e82c60f81d2427e5fff97d344e19f3006
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-29 01:01:36 +0700

    Add space before footer link

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ function Home() {
 			</div>
 			<div className="flex justify-center items-center py-8">
 				<p className="font-medium text-2xl text-light-20">
-					Profile picture by<a href={ Config.img.link } className="text-light-0">{ Config.img.creator }</a>.
+					Profile picture by <a href={ Config.img.link } className="text-light-0">{ Config.img.creator }</a>.
 				</p>
 			</div>
 		</div>
