10b91f664732d516e83b76d068f4f89b25ca89d5
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-29 11:29:13 +0700

    Move button element inside first flex

--- components/button.tsx
+++ components/button.tsx
@@ -16,14 +16,14 @@ function Button({ iconSlug, label, onClick }: IButtonProps) {
 			<div className="flex justify-end w-0.5 h-full">
 				<div className="bg-light-0" style={ { width: isHovered ? "0%" : "100%" } } />
 			</div>
-			<button type="button" onMouseOver={ () => setHovered(true) } onMouseOut={ () => setHovered(false) } onClick={ onClick } className="flex min-w-50 h-16">
-				<div className={ `flex items-center gap-x-4 px-6 py-4 ${ isHovered && "bg-light-0" }` }>
+			<div className="flex min-w-50 h-16">
+				<button type="button" onMouseOver={ () => setHovered(true) } onMouseOut={ () => setHovered(false) } onClick={ onClick } className={ `flex items-center gap-x-4 px-6 py-4 ${ isHovered && "bg-light-0" }` }>
 					<div className="h-9 aspect-square">
 						<SVG src={ getSimpleIconLink("jsdelivr", iconSlug) } className={ `h-9 ${ isHovered ? "fill-white" : "fill-black" }` } />
 					</div>
 					<span className={ `${ !isHovered && "hidden" } font-semibold text-2xl ${ isHovered && "text-white" }` }>{ label }</span>
-				</div>
-			</button>
+				</button>
+			</div>
 		</div>
 	);
 }
