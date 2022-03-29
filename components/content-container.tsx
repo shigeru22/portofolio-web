5f003030e728c3f022360a54e945a580cfbc6685
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-29 11:36:14 +0700

    Set content container min height to full

--- components/content-container.tsx
+++ components/content-container.tsx
@@ -4,7 +4,7 @@ interface IContentContainerProps {
 
 function ContentContainer({ children }: IContentContainerProps) {
 	return (
-		<div className="w-full space-y-4 pl-6 pt-2 pb-4 border-l-2 border-light-0">
+		<div className="min-h-full w-full space-y-4 pl-6 pt-2 pb-4 border-l-2 border-light-0">
 			{ children }
 		</div>
 	);
