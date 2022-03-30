52e2f6c98e4e38f90a27a39d0e83581113c98709
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-30 11:59:57 +0700

    Create screenshot component

---
+++ components/screenshot.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Image from "next/image";
+
+interface IScreenshotProps {
+	src: string;
+	alt: string;
+	description: string;
+	priority: boolean;
+}
+
+function Screenshot({ src, alt, description, priority }: IScreenshotProps) {
+	const [ ratio, setRatio ] = useState(16 / 9);
+
+	return (
+		<Image
+			src={ `/projects/${ src }` }
+			alt={ alt }
+			width={ ratio * 160 }
+			height={ 160 }
+			priority={ priority }
+			onLoadingComplete={ dimen => setRatio(dimen.naturalWidth / dimen.naturalHeight) } />
+	);
+}
+
+export default Screenshot;
