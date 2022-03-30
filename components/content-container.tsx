eb562dcbff07e66fec0f43ab127cc5e51f6499aa
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-30 16:38:23 +0700

    Implement content container back button

--- components/content-container.tsx
+++ components/content-container.tsx
@@ -1,12 +1,36 @@
+import { MouseEventHandler, useState } from "react";
+import { ChevronLeftIcon } from "@heroicons/react/outline";
+
 interface IContentContainerProps {
-	children: JSX.Element | JSX.Element[]
+	onBackClick?: MouseEventHandler<HTMLButtonElement>;
+	children: JSX.Element | JSX.Element[];
 }
 
-function ContentContainer({ children }: IContentContainerProps) {
+function ContentContainer({ onBackClick, children }: IContentContainerProps) {
+	const [ isHovered, setHovered ] = useState(false);
+
 	return (
-		<div className="min-h-full w-full space-y-4 pl-6 pt-2 pb-4 border-l-2 border-light-0">
-			{ children }
-		</div>
+		<>
+			{
+				typeof(onBackClick) !== "undefined" &&
+				<div className={ `relative flex ${ !isHovered && "border-l-2" } border-light-0` }>
+					<div className="flex justify-end w-0.5 h-full">
+						<div className="bg-light-0" style={ { width: isHovered ? "0%" : "100%" } } />
+					</div>
+					<div className="flex min-w-50 h-16">
+						<button type="button" onMouseOver={ () => setHovered(true) } onMouseOut={ () => setHovered(false) } onClick={ onBackClick } className={ `flex items-center gap-x-4 px-6 py-4 ${ isHovered && "bg-light-0" }` }>
+							<div className="h-9 aspect-square">
+								<ChevronLeftIcon className={ `h-9 stroke-1 ${ !isHovered ? "stroke-light-0" : "stroke-white" } aspect-square` } />
+							</div>
+							<span className={ `${ !isHovered && "hidden" } font-semibold text-2xl ${ isHovered && "text-white" }` }>Back</span>
+						</button>
+					</div>
+				</div>
+			}
+			<div className="min-h-full w-full space-y-4 pl-6 pt-2 pb-4 border-l-2 border-light-0">
+				{ children }
+			</div>
+		</>
 	);
 }
 
