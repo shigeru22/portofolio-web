df60804344a25e97598ab8bc3772ff118f6a6f46
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-03-31 17:38:11 +0700

    Fix project list hover stacking by limiting view

--- components/mobile/navbar.tsx
+++ components/mobile/navbar.tsx
@@ -7,7 +7,7 @@ interface IMobileNavbarProps {
 
 function MobileNavbar({ iconSrc }: IMobileNavbarProps) {
 	return (
-		<div className="flex justify-between items-center w-full h-24 px-8 py-6 bg-white z-10">
+		<div className="flex justify-between items-center w-full h-24 px-8 py-6 bg-white">
 			<SVG src={ iconSrc } className="w-8 h-8 fill-black" />
 			<button type="button">
 				<MenuAlt4Icon className="h-6 stroke-light-0" />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,10 @@ import MobileNavbar from "../components/mobile/navbar";
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<div className="flex flex-col w-screen h-screen">
-			<div className="fixed top-0 w-full">
+			<div className="top-0 w-full">
 				<MobileNavbar iconSrc="/kyuu.svg" />
 			</div>
-			<div className="h-full pt-24">
+			<div className="h-full overflow-y-auto">
 				<Component { ...pageProps } />
 			</div>
 		</div>
