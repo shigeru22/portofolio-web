c02ab9ea0671c23f4b95e8c0e05f27b1189963b0
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-06 10:51:29 +0700

    Rename bg to fill to mobile navbar SVG

--- components/mobile/navbar.tsx
+++ components/mobile/navbar.tsx
@@ -9,7 +9,7 @@ function MobileNavbar({ iconSrc }: IMobileNavbarProps) {
 	return (
 		<div className="flex justify-between items-center w-full h-24 px-8 py-6 bg-white dark:bg-black">
 			<Link href="/" passHref>
-				<SVG src={ iconSrc } className="w-8 h-8 fill-black dark:bg-white" />
+				<SVG src={ iconSrc } className="w-8 h-8 fill-black dark:fill-white" />
 			</Link>
 		</div>
 	);
