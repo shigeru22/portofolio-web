c5dbcbf46a2329a60d38500bae1ece4422dadadb
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-13 16:17:33 +0700

    Implement project item animations

--- components/project-item.tsx
+++ components/project-item.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 interface IProjectItemProps {
 	id: number;
@@ -13,19 +14,60 @@ function ProjectItem({ id, name, description, iconSrc }: IProjectItemProps) {
 	const [ isHovered, setHovered ] = useState(false);
 
 	return (
-		<div onMouseEnter={ () => setHovered(true) } onMouseLeave={ () => setHovered(false) } className="flex">
+		<div onMouseEnter={ () => setHovered(true) } onMouseLeave={ () => setHovered(false) } className="flex cursor-pointer">
 			<div className="flex justify-end w-0.5 min-h-24">
-				<div className="bg-light-0 dark:bg-dark-100" style={ { width: isHovered ? "0%" : "100%" } } />
+				<motion.div
+					animate={ isHovered ? "hovered" : "default" }
+					variants={ {
+						hovered: {
+							width: "0%",
+							transition: {
+								duration: 0.15,
+								ease: "easeOut"
+							}
+						},
+						default: {
+							width: "100%",
+							transition: {
+								duration: 0.15,
+								ease: "easeOut"
+							}
+						}
+					} }
+					className="bg-light-0 dark:bg-dark-100" />
 			</div>
 			<div className="flex w-full min-h-24">
 				<Link href={ `/portfolio/${ id }` } passHref>
-					<button type="button" className={ `flex items-center gap-x-4 h-full px-6 ${ isHovered && "bg-light-0 dark:bg-dark-100" } -z-20` }>
-						<div className="relative h-12 aspect-square -z-10">
-							<Image src={ iconSrc } alt={ `${ name } icon` } layout="fill" objectFit="contain" />
+					<button type="button" className="relative w-full h-full -z-20">
+						<div className="absolute top-0 left-0 w-full h-full -z-30">
+							<motion.div
+								animate={ isHovered ? "hovered" : "default" }
+								variants={ {
+									hovered: {
+										width: "100%",
+										transition: {
+											duration: 0.15,
+											ease: "easeOut"
+										}
+									},
+									default: {
+										width: "0%",
+										transition: {
+											duration: 0.15,
+											ease: "easeOut"
+										}
+									}
+								} }
+								className="h-full bg-light-0 dark:bg-dark-100" />
 						</div>
-						<div className="flex flex-col gap-y-1 -z-10">
-							<h3 className={ `font-semibold text-left text-xl md:text-2xl ${ !isHovered ? "text-light-0 dark:text-dark-100" : "text-white dark:text-dark-0" } leading-tight md:leading-tight` }>{ name }</h3>
-							<h4 className={ `text-left md:text-lg ${ !isHovered ? "text-light-0 dark:text-dark-100" : "text-white dark:text-dark-0" } leading-tight md:leading-none` }>{ description }</h4>
+						<div className="absolute top-0 left-0 flex items-center gap-x-4 h-full px-6">
+							<div className="relative h-12 aspect-square -z-10">
+								<Image src={ iconSrc } alt={ `${ name } icon` } layout="fill" objectFit="contain" />
+							</div>
+							<div className="flex flex-col gap-y-1 -z-10">
+								<h3 className={ `font-semibold text-left text-xl md:text-2xl ${ !isHovered ? "text-light-0 dark:text-dark-100" : "text-white dark:text-dark-0" } leading-tight md:leading-tight` }>{ name }</h3>
+								<h4 className={ `text-left md:text-lg ${ !isHovered ? "text-light-0 dark:text-dark-100" : "text-white dark:text-dark-0" } leading-tight md:leading-none` }>{ description }</h4>
+							</div>
 						</div>
 					</button>
 				</Link>
