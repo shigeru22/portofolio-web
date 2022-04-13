4fd31cae276274b952e5a82a01117ea04595e425
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-13 16:06:44 +0700

    Move project item hover element

--- components/project-item.tsx
+++ components/project-item.tsx
@@ -13,17 +13,17 @@ function ProjectItem({ id, name, description, iconSrc }: IProjectItemProps) {
 	const [ isHovered, setHovered ] = useState(false);
 
 	return (
-		<div className="flex">
+		<div onMouseEnter={ () => setHovered(true) } onMouseLeave={ () => setHovered(false) } className="flex">
 			<div className="flex justify-end w-0.5 min-h-24">
 				<div className="bg-light-0 dark:bg-dark-100" style={ { width: isHovered ? "0%" : "100%" } } />
 			</div>
 			<div className="flex w-full min-h-24">
 				<Link href={ `/portfolio/${ id }` } passHref>
-					<button type="button" onMouseOver={ () => setHovered(true) } onMouseOut={ () => setHovered(false) } className={ `flex items-center gap-x-4 h-full px-6 ${ isHovered && "bg-light-0 dark:bg-dark-100" }` }>
-						<div className="relative h-12 aspect-square">
-							<Image src={ iconSrc } alt={ `${ name } icon` } layout="fill" className={ `${ !isHovered && "-z-10" }` } />
+					<button type="button" className={ `flex items-center gap-x-4 h-full px-6 ${ isHovered && "bg-light-0 dark:bg-dark-100" } -z-20` }>
+						<div className="relative h-12 aspect-square -z-10">
+							<Image src={ iconSrc } alt={ `${ name } icon` } layout="fill" objectFit="contain" />
 						</div>
-						<div className={ `${ !isHovered && "lg:invisible" } flex flex-col gap-y-1` }>
+						<div className="flex flex-col gap-y-1 -z-10">
 							<h3 className={ `font-semibold text-left text-xl md:text-2xl ${ !isHovered ? "text-light-0 dark:text-dark-100" : "text-white dark:text-dark-0" } leading-tight md:leading-tight` }>{ name }</h3>
 							<h4 className={ `text-left md:text-lg ${ !isHovered ? "text-light-0 dark:text-dark-100" : "text-white dark:text-dark-0" } leading-tight md:leading-none` }>{ description }</h4>
 						</div>
