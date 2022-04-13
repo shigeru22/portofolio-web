0e89e1d390776839fe2754a075e8239c89e1a1ec
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-13 16:54:33 +0700

    Animate language graph on view

--- components/language-graph.tsx
+++ components/language-graph.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { ValueError } from "../types/error";
 import { ILanguageGraphData } from "../types/components/language-graph";
 
@@ -21,7 +22,24 @@ function LanguageGraph({ data }: ILanguageGraphProps) {
 		return (
 			<div className="flex flex-col gap-y-2 w-14 2xl:w-16 h-full border-b-2 border-light-0 dark:border-dark-100">
 				<div className="flex flex-grow justify-center items-end">
-					<div title={ `${ heightPercentage }%` } className="w-2 2xl:w-3 bg-light-20 dark:bg-dark-80" style={ { height: `${ heightPercentage }%` } } />
+					<motion.div
+						initial="hidden"
+						whileInView="shown"
+						viewport={ { once: true } }
+						variants={ {
+							hidden: {
+								height: "0%"
+							},
+							shown: {
+								height: `${ heightPercentage }%`,
+								transition: {
+									duration: 0.6,
+									ease: "backOut"
+								}
+							}
+						} }
+						title={ `${ heightPercentage }%` }
+						className="w-2 2xl:w-3 bg-light-20 dark:bg-dark-80" />
 				</div>
 				<div className="flex justify-center items-center w-full h-8">
 					<p className="font-medium text-lg 2xl:text-xl text-light-0 dark:text-dark-100">{ label }</p>
